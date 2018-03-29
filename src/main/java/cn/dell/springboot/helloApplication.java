e1645020bef582336cb5db526b9c9da0dcf8d2e6
592593695@qq.com <37507486+ReaderHu@users.noreply.github.com>
2018-03-29 21:55:48 +0800

    up

---
+++ src/main/java/cn/dell/springboot/helloApplication.java
@@ -0,0 +1,28 @@
+package cn.dell.springboot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+@Configuration
+public class helloApplication {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        SpringApplication.run(helloApplication.class, args);
+
+    }
+
+    @RequestMapping("hello")
+    @ResponseBody
+    public String hello() {
+        return "hello worlds";
+    }
+
+}
