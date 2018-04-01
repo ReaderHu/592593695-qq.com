07b20af3eb1c4ff27fb36e5d629f1a89c2e42c8b
592593695@qq.com <37507486+ReaderHu@users.noreply.github.com>
2018-04-01 17:44:30 +0800

    up

--- src/main/java/cn/dell/springboot/helloApplication.java
+++ src/main/java/cn/dell/springboot/helloApplication.java
@@ -4,10 +4,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 public class helloApplication {
-    @ResponseBody
-    @RequestMapping("hello")
-    private String hello(){
-        return "hello world";
 
-    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(demoApplication.class, args);
+	}
+
+	@ResponseBody
+	@RequestMapping("hello")
+	private String hello() {
+		return "hello world";
+
+	}
 }
